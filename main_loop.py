146876e55e948e9a888c17d7f5c8b1f8b95bbc19
arstrim <arstrim97@gmail.com>
2020-06-14 19:05:25 +0300

    identation

--- main_loop.py
+++ main_loop.py
@@ -2,12 +2,12 @@ import sys
 import urllib.request
 
 if __name__ == '__main__':
-	urls = sys.argv[1].split(",")
-	for url in urls:
-		print(url)
+    urls = sys.argv[1].split(",")
+    for url in urls:
+        print(url)
         try:
-		    r = urllib.request.urlopen(url).read()
-		    print(r)
+            r = urllib.request.urlopen(url).read()
+            print(r)
         except ValueError:
             print("Not posible to parse html")
             sys.exit()
