badd511c87342a1090f3f9aa285ab411b7724c9b
arstrim <arstrim97@gmail.com>
2020-06-14 18:47:49 +0300

    with html

--- main_loop.py
+++ main_loop.py
@@ -1,6 +1,11 @@
 import sys
+import urlib.requests
 
 if __name__ == '__main__':
     for arg in sys.argv[1].split(","):
         print(arg)
+        r = urlib.request.urlopen(arg).read()
+        print(r)
+
+
 
