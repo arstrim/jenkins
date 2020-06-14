7cb30cf1a69f4025c8c1e4813f6d1079fc83af57
arstrim <arstrim97@gmail.com>
2020-06-14 18:53:38 +0300

    main

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM python:3.6-jessie
 WORKDIR /opt
 ADD / /opt
 RUN pip install -r requirements.txt
-ENV URL="https://www.google.com, https://facebook.com, https://youtube.com"
+ENV URL="https://www.opentable.com, https://www.opentable.com/m/best-restaurants-in-america-for-2017/"
 
 ENTRYPOINT python -u /opt/main.py $URL
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+# import sys
+#
+# if __name__ == '__main__':
+# 	num = int(sys.argv[1])
+# 	for i in range(num):
+# 		print(i, i*i)
+
+
 import sys
+import urlib.requests
 
 if __name__ == '__main__':
-	num = int(sys.argv[1])
-	for i in range(num):
-		print(i, i*i)
+    for arg in sys.argv[1].split(","):
+        print(arg)
+        r = urlib.request.urlopen(arg).read()
+        print(r)
 
