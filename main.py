436735afeb8183e2333c98484f6222e38c53ea52
arstrim <arstrim97@gmail.com>
2020-06-14 18:57:46 +0300

    url variable

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import sys
 import urllib.request
 
 if __name__ == '__main__':
-    for arg in sys.argv[1].split(","):
-        print(arg)
-        r = urllib.request.urlopen(arg).read()
+	urls = sys.argv[1].split(",")
+    for url in urls:
+        print(url)
+        r = urllib.request.urlopen(url).read()
         print(r)
 
