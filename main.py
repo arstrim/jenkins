bb17692af146439aee178484dc793a3ecbf8e5ce
arstrim <arstrim97@gmail.com>
2020-06-14 18:43:17 +0300

    loop split ,

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 if __name__ == '__main__':
-	num = int(sys.argv[1]).split(,)
+	num = int(sys.argv[1])
 	for i in range(num):
 		print(i, i*i)
 
--- main_loop.py
+++ main_loop.py
@@ -1,6 +1,6 @@
 import sys
 
 if __name__ == '__main__':
-    for arg in sys.argv[1:]:
+    for arg in sys.argv[1].split(","):
         print(arg)
 
